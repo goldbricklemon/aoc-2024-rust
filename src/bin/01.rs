4900c5a99cdc8f2a3fdef2a5db503037dbdfec98
goldbricklemon <9368670+goldbricklemon@users.noreply.github.com>
2024-12-05 01:10:25 +0100

    Just some comments and refinement to Day 01

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -26,26 +26,40 @@ fn main() -> Result<()> {
     //region Part 1
     println!("=== Part 1 ===");
 
-    fn input_to_vec_pair<R: BufRead>(reader: R) -> Result<(Vec<i64>, Vec<i64>)> {
-        let mut left: Vec<i64> = Vec::new();
-        let mut right: Vec<i64> = Vec::new();
+    ///
+    /// Read input file and map to left and right vectors of numbers
+    ///
+    fn input_to_vec_pair<R: BufRead>(reader: R) -> Result<(Vec<u64>, Vec<u64>)> {
+        let mut left: Vec<u64> = Vec::new();
+        let mut right: Vec<u64> = Vec::new();
         for line in reader.lines() {
             let line = line?;
             let parts: Vec<&str> = line.split("   ").collect();
             assert_eq!(parts.len(), 2);
-            left.push(i64::from_str(parts[0])?);
-            right.push(i64::from_str(parts[1])?);
+            left.push(u64::from_str(parts[0])?);
+            right.push(u64::from_str(parts[1])?);
         }
         Ok((left, right))
     }
 
-    fn part1<R: BufRead>(reader: R) -> Result<i64> {
+    fn part1<R: BufRead>(reader: R) -> Result<u64> {
         let (mut left, mut right) = input_to_vec_pair(reader)?;
+        // Sort
         left.sort();
         right.sort();
+        // Build pair iterator
         let pairs = zip(left.iter(), right.iter());
-        let diffs = pairs.map(|pair| (pair.0 - pair.1).abs());
-        let sum: i64 = diffs.fold(0, |a, b| a + b);
+        // Map pairs to abs diffs
+        fn abs_diff(a: u64, b: u64) -> u64 {
+            if a > b {
+                a - b
+            } else {
+                b - a
+            }
+        }
+        let diffs = pairs.map(|pair| abs_diff(*pair.0, *pair.1));
+        // Sum up
+        let sum: u64 = diffs.fold(0, |a, b| a + b);
         Ok(sum)
     }
 
@@ -60,12 +74,15 @@ fn main() -> Result<()> {
     //region Part 2
      println!("\n=== Part 2 ===");
 
-     fn part2<R: BufRead>(reader: R) -> Result<i64> {
+     fn part2<R: BufRead>(reader: R) -> Result<u64> {
 
          let (mut left, mut right) = input_to_vec_pair(reader)?;
          // Let's do this the inefficient way.
          // No sorting, no potential re-using of similarity scores
-         let sim_scores = left.iter().map(|l| *l * i64::try_from(right.iter().filter(|r| **r == *l).count()).unwrap()).fold(0, |a, b| a + b );
+         fn count_in_right(x: u64, right: &Vec<u64>) -> u64  {
+             u64::try_from(right.iter().filter(|r| **r == x).count()).unwrap()
+         }
+         let sim_scores = left.iter().map(|l| *l * count_in_right(*l, &right)).fold(0, |a, b| a + b);
 
          Ok(sim_scores)
      }
